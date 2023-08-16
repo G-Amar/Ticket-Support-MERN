1fa89a84c05e0d8f4d9385f91ea8d74eeefbf5da
G-Amar <amar@LAPTOP-156VA6C4>
2023-08-15 21:14:11 -0400

    added DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 8000
+
+// connect to the database
+connectDB()
 
 const app = express()
 
