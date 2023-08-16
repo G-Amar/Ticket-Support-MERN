a920f411a546d9842449487db7717bced0331665
G-Amar <amar@LAPTOP-156VA6C4>
2023-08-15 22:17:45 -0400

    added token protected paths

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 //logic in separate file
-const {registerUser, loginUser} = require('../controllers/userControler')
+const {registerUser, loginUser, getMe} = require('../controllers/userControler')
+
+const {protect} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
 router.post('/login', loginUser)
+router.get('/me', protect, getMe) //protect acts as a guard
 
 module.exports = router 
