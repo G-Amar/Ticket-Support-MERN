d409235cafb52145e7c0d9deb1b78a5f238d5bf2
G-Amar <amar@LAPTOP-156VA6C4>
2023-08-15 21:07:39 -0400

    add post request input

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 
 const express = require('express')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
 const PORT = process.env.PORT || 8000
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
   res.status(200).send('hello')
 })
 
 app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`)
